8ee58da5fc829852b85d795205db922a33559280
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-09-25 06:40:29 +0300

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibrary1
+{
+    //Generic Class
+    public class User<T1, T2>
+    {
+        //generic field
+        public T1 RegistrationStatus;
+
+        //second generic field
+        public T2 Age;
+    }
+}
---
+++ GenericClassExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using ClassLibrary1;
+
+namespace GenericClassExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User<int, int> user1 = new User<int, int>();
+            User<bool, string> user2 = new User<bool, string>();
+
+            //set value into generic field
+            user1.RegistrationStatus = 1234;
+            user2.RegistrationStatus = false;
+
+            user1.Age = 10;
+            user2.Age = "35 - 40";
+
+            Console.WriteLine(user1.RegistrationStatus);
+            Console.WriteLine(user2.RegistrationStatus);
+
+            Console.WriteLine(user1.Age);
+            Console.WriteLine(user2.Age);
+
+            Console.ReadKey();
+        }
+    }
+}
